aa48d73ebdc9bb5f365087a15686350aaa2eee59
core2drew <calupesandro@gmail.com>
2017-06-11 22:51:31 +0800

    add compare image images

--- nav_contents/compare-images.php
+++ nav_contents/compare-images.php
@@ -1,7 +1,7 @@
 <?php
 if ($var == 'og_desc') {
-$og_desc = 'Dis aliquet aliquam in sit ridiculus dolor tortor ridiculus, augue. Tincidunt. Lectus ac nec, risus! Pid, nisi purus nisi? Augue augue nisi penatibus. Purus, sagittis amet velit! Dapibus magna rhoncus, scelerisque, nec ac odio velit lacus et mid urna natoque integer porta mattis, nisi, aliquet nunc porttitor, a eu nec odio, adipiscing, quis dictumst eros urna est egestas parturient.';
-return $og_desc;
+    $og_desc = 'Dis aliquet aliquam in sit ridiculus dolor tortor ridiculus, augue. Tincidunt. Lectus ac nec, risus! Pid, nisi purus nisi? Augue augue nisi penatibus. Purus, sagittis amet velit! Dapibus magna rhoncus, scelerisque, nec ac odio velit lacus et mid urna natoque integer porta mattis, nisi, aliquet nunc porttitor, a eu nec odio, adipiscing, quis dictumst eros urna est egestas parturient.';
+    return $og_desc;
 }
 ?>
 <section id="about-state" class="container content-st-section">
@@ -11,22 +11,30 @@ return $og_desc;
             <div class="section-title fc-dark-brown">Compare images of branded and plain packs</div>
         </div>
         <div class="content-desc-cont col-lg-10 col-lg-offset-1">
-            <div class="section-secondary-title fc-dark-brown">Dis aliquet aliquam in sit ridiculus dolor
-                tortor ridiculus, augue. Tincidunt. Lectus ac nec, risus! Pid, nisi purus nisi? Augue augue
-                nisi penatibus. Purus, sagittis amet velit! Dapibus magna rhoncus, scelerisque, nec ac odio
-                velit lacus et mid urna natoque integer porta mattis, nisi, aliquet nunc porttitor, a eu nec
-                odio, adipiscing, quis dictumst eros urna est egestas parturient.
+            <div class="row text-center">
+                <p class="col-lg-3">Plain Packaging</p>
+                <p class="col-lg-3">Branded Packaging</p>
             </div>
-            <p>Nec lacus aliquet aliquet elit massa, velit, aliquam ac quis in rhoncus in, turpis! Rhoncus
-                ridiculus proin, vel duis elementum risus adipiscing sed dapibus! Placerat nunc, urna. Et
-                proin. Lacus mattis! Ridiculus porta scelerisque auctor tempor magna auctor! Ut, porttitor
-                enim lundium elementum cras, pulvinar dis magna, porttitor urna! Nec? Nascetur lorem in et
-                phasellus odio aenean porta lorem.</p>
-            <p>Et porttitor porta porta dignissim lacus? Urna urna natoque tincidunt scelerisque! Nisi.
-                Dolor? Cum nascetur! Nisi penatibus porta a. Vel et aenean, dignissim. Nunc turpis,
-                sagittis, placerat proin? Mid? Ridiculus scelerisque integer, auctor turpis nunc? Tincidunt
-                rhoncus enim cras tincidunt adipiscing? Et vel magna, est, vel nunc? Ultricies ac nisi, ac
-                proin parturient ultricies et tempor tincidunt magna odio.</p>
+            <div class="row">
+                <img src="<?php echo $base_url; ?>img/compare-image/plains/Chesterfield plain.jpg" class="col-lg-3">
+                <img src="<?php echo $base_url; ?>img/compare-image/brands/Chesterfield brand.jpg" class="col-lg-3">
+            </div>
+            <div class="row">
+                <img src="<?php echo $base_url; ?>img/compare-image/plains/dunhill plain.jpg" class="col-lg-3">
+                <img src="<?php echo $base_url; ?>img/compare-image/brands/Dunhill brand.jpg" class="col-lg-3">
+            </div>
+            <div class="row">
+                <img src="<?php echo $base_url; ?>img/compare-image/plains/gauloises plain.jpg" class="col-lg-3">
+                <img src="<?php echo $base_url; ?>img/compare-image/brands/gauloises branded.jpg" class="col-lg-3">
+            </div>
+            <div class="row">
+                <img src="<?php echo $base_url; ?>img/compare-image/plains/lambert and butler plain.jpg" class="col-lg-3">
+                <img src="<?php echo $base_url; ?>img/compare-image/brands/Lmbert and butler banded.jpg" class="col-lg-3">
+            </div>
+            <div class="row">
+                <img src="<?php echo $base_url; ?>img/compare-image/plains/marlboro plain.png" class="col-lg-3">
+                <img src="<?php echo $base_url; ?>img/compare-image/brands/marlboro branded.png" class="col-lg-3">
+            </div>
         </div>
     </div>
 </section>
